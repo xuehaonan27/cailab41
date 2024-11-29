dead75e2455bd5051134e088eccc1313e47c2cef
Xue Haonan <xuehaonan27@gmail.com>
2024-11-29 16:46:01 +0000

    feat: better macro for YUV and RGB conversion

--- cpp/plain.cc
+++ cpp/plain.cc
@@ -86,13 +86,20 @@ void solve_plain_int(
                 const uint8_t u = u_data[uv_index];
                 const uint8_t v = v_data[uv_index];
 
-// YUV -> RGB
-#define YUV2R(y, u, v) clamp((298 * ((y) - 16) + 409 * ((v) - 128) + 128) >> 8)
-#define YUV2G(y, u, v) clamp((298 * ((y) - 16) - 100 * ((u) - 128) - 208 * ((v) - 128) + 128) >> 8)
-#define YUV2B(y, u, v) clamp((298 * ((y) - 16) + 516 * ((u) - 128) + 128) >> 8)
+                // YUV -> RGB
+                // #define YUV2R(y, u, v) clamp((298 * ((y) - 16) + 409 * ((v) - 128) + 128) >> 8)
+                // #define YUV2G(y, u, v) clamp((298 * ((y) - 16) - 100 * ((u) - 128) - 208 * ((v) - 128) + 128) >> 8)
+                // #define YUV2B(y, u, v) clamp((298 * ((y) - 16) + 516 * ((u) - 128) + 128) >> 8)
+                // #define YUV2R(y, u, v) clamp(((42 * ((y) - 16) + 153 * ((v) - 128) + 128) >> 8) + ((y) - 16) + ((v) - 128))
+                // #define YUV2G(y, u, v) clamp((((42 * ((y) - 16) - 100 * ((u) - 128)) + (128 - 208 * ((v) - 128))) >> 8) + ((y) - 16))
+                // #define YUV2B(y, u, v) clamp(((42 * ((y) - 16) + 4 * ((u) - 128) + 128) >> 8) + ((y) - 16) + 2 * ((u) - 128))
                 // #define YUV2R(y, u, v) clamp((298 * (y) + 409 * (v) - 56992) >> 8)
                 // #define YUV2G(y, u, v) clamp((298 * (y) - 100 * (u) - 208 * (v) + 34784) >> 8)
                 // #define YUV2B(y, u, v) clamp((298 * (y) + 516 * (u) - 70688) >> 8)
+
+#define YUV2R(y, u, v) clamp(((42 * (y) - 24224 + 153 * (v)) >> 8) + ((y) - 128) + (v))
+#define YUV2G(y, u, v) clamp(((42 * (y) - 100 * (u) + 2016 - 208 * (v)) >> 8) + ((y) + 128))
+#define YUV2B(y, u, v) clamp(((42 * (y) + 4 * (u) - 5152) >> 8) + ((y) - 128) + (2 * (u) - 128))
 
                 const uint8_t r = YUV2R(y, u, v) & 0xff;
                 const uint8_t g = YUV2G(y, u, v) & 0xff;
@@ -104,9 +111,12 @@ void solve_plain_int(
                 const uint16_t b2 = alpha * b >> 8;
 
 // RGB -> YUV
-#define RGB2Y(r, g, b) (uint8_t)(((66 * (r) + 129 * (g) + 25 * (b) + 128) >> 8) + 16)
-#define RGB2U(r, g, b) (uint8_t)(((-38 * (r) - 74 * (g) + 112 * (b) + 128) >> 8) + 128)
-#define RGB2V(r, g, b) (uint8_t)(((112 * (r) - 94 * (g) - 18 * (b) + 128) >> 8) + 128)
+// #define RGB2Y(r, g, b) (uint8_t)(((66 * (r) + 129 * (g) + 25 * (b) + 128) >> 8) + 16)
+// #define RGB2U(r, g, b) (uint8_t)(((-38 * (r) - 74 * (g) + 112 * (b) + 128) >> 8) + 128)
+// #define RGB2V(r, g, b) (uint8_t)(((112 * (r) - 94 * (g) - 18 * (b) + 128) >> 8) + 128)
+#define RGB2Y(r, g, b) (uint8_t)((((66 * (r) - 8192) + (129 * (g) - 16384) + (25 * (b) - 1920)) >> 8) + 120)
+#define RGB2U(r, g, b) (uint8_t)((((112 * (b) - 38 * (r)) + (128 - 74 * (g))) >> 8) + 128)
+#define RGB2V(r, g, b) (uint8_t)((((112 * (r) - 94 * (g)) + (128 - 18 * (b))) >> 8) + 128)
                 uint8_t y2 = RGB2Y(r2, g2, b2);
                 uint8_t u2 = RGB2U(r2, g2, b2);
                 uint8_t v2 = RGB2V(r2, g2, b2);
@@ -148,9 +158,9 @@ void solve_plain_part3(
                 const uint8_t p2_u = p2_u_data[uv_index];
                 const uint8_t p2_v = p2_v_data[uv_index];
 
-#define YUV2R(y, u, v) clamp((298 * ((y) - 16) + 409 * ((v) - 128) + 128) >> 8)
-#define YUV2G(y, u, v) clamp((298 * ((y) - 16) - 100 * ((u) - 128) - 208 * ((v) - 128) + 128) >> 8)
-#define YUV2B(y, u, v) clamp((298 * ((y) - 16) + 516 * ((u) - 128) + 128) >> 8)
+                // #define YUV2R(y, u, v) clamp((298 * ((y) - 16) + 409 * ((v) - 128) + 128) >> 8)
+                // #define YUV2G(y, u, v) clamp((298 * ((y) - 16) - 100 * ((u) - 128) - 208 * ((v) - 128) + 128) >> 8)
+                // #define YUV2B(y, u, v) clamp((298 * ((y) - 16) + 516 * ((u) - 128) + 128) >> 8)
                 const uint8_t p1_r = YUV2R(p1_y, p1_u, p1_v) & 0xff;
                 const uint8_t p1_g = YUV2G(p1_y, p1_u, p1_v) & 0xff;
                 const uint8_t p1_b = YUV2B(p1_y, p1_u, p1_v) & 0xff;
@@ -168,9 +178,9 @@ void solve_plain_part3(
                 const uint16_t b_prime = MIX(alpha, p1_b, p2_b);
 
                 // RGB -> YUV
-#define RGB2Y(r, g, b) (uint8_t)(((66 * (r) + 129 * (g) + 25 * (b) + 128) >> 8) + 16)
-#define RGB2U(r, g, b) (uint8_t)(((-38 * (r) - 74 * (g) + 112 * (b) + 128) >> 8) + 128)
-#define RGB2V(r, g, b) (uint8_t)(((112 * (r) - 94 * (g) - 18 * (b) + 128) >> 8) + 128)
+                // #define RGB2Y(r, g, b) (uint8_t)(((66 * (r) + 129 * (g) + 25 * (b) + 128) >> 8) + 16)
+                // #define RGB2U(r, g, b) (uint8_t)(((-38 * (r) - 74 * (g) + 112 * (b) + 128) >> 8) + 128)
+                // #define RGB2V(r, g, b) (uint8_t)(((112 * (r) - 94 * (g) - 18 * (b) + 128) >> 8) + 128)
                 uint8_t y_prime = RGB2Y(r_prime, g_prime, b_prime);
                 uint8_t u_prime = RGB2U(r_prime, g_prime, b_prime);
                 uint8_t v_prime = RGB2V(r_prime, g_prime, b_prime);
